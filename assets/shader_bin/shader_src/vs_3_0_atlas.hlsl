cb95b9446f14c9bc8754837e7d113ce7a7a396a9
Iswenzz <suxlolz1528@gmail.com>
2022-10-16 14:15:19 +0200

    Shader: Atlas

---
+++ assets/shader_bin/shader_src/vs_3_0_atlas.hlsl
@@ -0,0 +1,37 @@
+#define PC
+#define IS_VERTEX_SHADER 1
+#define IS_PIXEL_SHADER 0
+#include <common.hlsl>
+
+struct VertexShaderInput
+{
+	float4 position : POSITION;
+	float4 uv : TEXCOORD0;
+};
+
+struct PixelShaderInput
+{
+	float4 position : POSITION;
+	float2 uv : TEXCOORD0;
+};
+
+PixelShaderInput vs_main(VertexShaderInput input)
+{
+	PixelShaderInput output;
+	output.position = mul(float4(input.position.xyz, 1.0f), worldViewProjectionMatrix);
+
+	const float SPRITE_COLUMNS = 12.0;
+	const float SPRITE_ROWS = 12.0;
+	const int NUM_OF_SPRITES = SPRITE_COLUMNS * SPRITE_ROWS;
+	const int speed = 10;
+
+	int index = int(gameTime.w * speed) % NUM_OF_SPRITES;
+	float2 pos = float2(index % int(SPRITE_COLUMNS), int(index / SPRITE_COLUMNS));
+	float2 uv = computeTextureUV(input.uv);
+
+	output.uv = float2(
+		(uv.x / SPRITE_COLUMNS) + pos.x * (1.0 / SPRITE_COLUMNS),
+        (uv.y / SPRITE_ROWS) + pos.y * (1.0 / SPRITE_ROWS));
+
+	return output;
+}
